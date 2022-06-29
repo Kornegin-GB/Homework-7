90125565e2540880ad2f94a58ba411ba8363d9b3
Sergey Filippenkov <fsvmxd@yandex.ru>
2022-06-29 22:12:46 +0300

    Решение задачи 50

--- Task-50/Program.cs
+++ Task-50/Program.cs
@@ -5,3 +5,30 @@
 // 8 4 2 4
 // 17 -> такого числа в массиве нет
 
+using System;
+using static System.Console;
+using MyLibs;
+Clear();
+
+Write("Введите количество строк массива (m): ");
+int m = Convert.ToInt32(ReadLine());
+Write("Введите количество столбцов массива (n): ");
+int n = Convert.ToInt32(ReadLine());
+
+Write("Введите номе ряда для поиска: ");
+int row = Convert.ToInt32(ReadLine());
+Write("Введите номе колонки для поиска: ");
+int column = Convert.ToInt32(ReadLine());
+
+int[,] arr = new int[m, n];
+MyMetods.GetRandomArrayInt(arr);
+MyMetods.PrintArrayInt(arr);
+
+if (row < m && row >= 0 && column < n && column >= 0)
+{
+   WriteLine($"Число в ряду {row} и столбце {column} -> {arr[row, column]}");
+}
+else
+{
+   WriteLine($"{row}, {column} -> Такого числа в массиве нет");
+}
