9db35f4753766c6980cfa1f18d978e4a2f268fdc
Sergey Filippenkov <fsvmxd@yandex.ru>
2022-06-29 21:43:17 +0300

    Решение задачи 47

--- MyLibs/MyMetods.cs
+++ MyLibs/MyMetods.cs
@@ -1,5 +1,49 @@
 ﻿namespace MyLibs;
 public class MyMetods
 {
+   public static void GetRandomArrayDouble(double[,] array)
+   {
+      for (int i = 0; i < array.GetLength(0); i++)
+      {
+         for (int j = 0; j < array.GetLength(1); j++)
+         {
+            array[i, j] = new Random().NextDouble() * 20 - 10;
+         }
+      }
+   }
 
+   public static void PrintArrayDouble(double[,] array)
+   {
+      for (int i = 0; i < array.GetLength(0); i++)
+      {
+         for (int j = 0; j < array.GetLength(1); j++)
+         {
+            Console.Write($"{array[i, j]:f1} ");
+         }
+         Console.WriteLine();
+      }
+   }
+
+   public static void GetRandomArrayInt(int[,] array)
+   {
+      for (int i = 0; i < array.GetLength(0); i++)
+      {
+         for (int j = 0; j < array.GetLength(1); j++)
+         {
+            array[i, j] = new Random().Next(-10, 10);
+         }
+      }
+   }
+
+   public static void PrintArrayInt(int[,] array)
+   {
+      for (int i = 0; i < array.GetLength(0); i++)
+      {
+         for (int j = 0; j < array.GetLength(1); j++)
+         {
+            Console.Write($"{array[i, j]} ");
+         }
+         Console.WriteLine();
+      }
+   }
 }
--- Task-47/Program.cs
+++ Task-47/Program.cs
@@ -4,3 +4,16 @@
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
 
+using System;
+using static System.Console;
+using MyLibs;
+Clear();
+
+Write("Введите количество строк массива (m): ");
+int m = Convert.ToInt32(ReadLine());
+Write("Введите количество столбцов массива (n): ");
+int n = Convert.ToInt32(ReadLine());
+
+double[,] arr = new double[m, n];
+MyMetods.GetRandomArrayDouble(arr);
+MyMetods.PrintArrayDouble(arr);
